7dd4f424d199cad125de73b47040d8619808e5a3
Art Hyena <90830990+Artechigh@users.noreply.github.com>
2023-02-06 23:45:38 +0000

    now recognizes special characters!

--- prog.cpp
+++ prog.cpp
@@ -1,6 +1,7 @@
 using namespace std;
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 struct question{
     question() {
@@ -9,6 +10,20 @@ struct question{
     int number;
     string name;
 };
+
+// stackoverflow
+vector<int> findLocation(string sample, char findIt)
+{
+    vector<int> characterLocations;
+    for(int i =0; i < sample.size(); i++)
+        if(sample[i] == findIt)
+            characterLocations.push_back(i);
+
+    return characterLocations;
+}
+// end of stackoverflow
+
+
 int main() {
     fstream fIn, fOut;
     fIn.open("textIn.txt");
@@ -16,23 +31,63 @@ int main() {
     string sIn="lol"; // string In
     question qIn; // question in
     int plCount = 0; // plus's counter
+    vector<int> specialSymbolsLocationsInStringContainer;
     string answer;
-    getline(fIn,sIn); // got 1-st line
+    int stringCounter = 0; //!!!!!
+    getline(fIn,sIn);
+    stringCounter++;//!!!!!!
     while (sIn[0]==' ') {sIn.erase(0,1);}// clear leading spaces
     while (sIn[sIn.length()-1] == ' ') {sIn.pop_back();} // clear trailing spaces
 
-    while (sIn!="") { // while there are no empty strings
-        if (sIn[sIn.length()-1] == '?' or sIn[sIn.length()-1] == ':' or sIn[sIn.length()-1] == '.') { // if found question text then type it out
+    // MADNESS STARTS!
+
+    while (sIn!="") {
+        specialSymbolsLocationsInStringContainer = findLocation(sIn,'~');
+        while (specialSymbolsLocationsInStringContainer.size()!=0) {
+            sIn.insert(specialSymbolsLocationsInStringContainer.back(),"\\");
+            specialSymbolsLocationsInStringContainer.pop_back();
+        }
+        specialSymbolsLocationsInStringContainer = findLocation(sIn,'=');
+        while (specialSymbolsLocationsInStringContainer.size()!=0) {
+            //cout << stringCounter <<" "<< specialSymbolsLocationsInStringContainer.back() << endl; // for debug
+            sIn.insert(specialSymbolsLocationsInStringContainer.back(),"\\");
+            specialSymbolsLocationsInStringContainer.pop_back();
+        }
+        specialSymbolsLocationsInStringContainer = findLocation(sIn,'#');
+        while (specialSymbolsLocationsInStringContainer.size()!=0) {
+            sIn.insert(specialSymbolsLocationsInStringContainer.back(),"\\");
+            specialSymbolsLocationsInStringContainer.pop_back();
+        }
+        specialSymbolsLocationsInStringContainer = findLocation(sIn,'{');
+        while (specialSymbolsLocationsInStringContainer.size()!=0) {
+            sIn.insert(specialSymbolsLocationsInStringContainer.back(),"\\");
+            specialSymbolsLocationsInStringContainer.pop_back();
+        }
+        specialSymbolsLocationsInStringContainer = findLocation(sIn,'}');
+        while (specialSymbolsLocationsInStringContainer.size()!=0) {
+            sIn.insert(specialSymbolsLocationsInStringContainer.back(),"\\");
+            specialSymbolsLocationsInStringContainer.pop_back();
+        }
+        specialSymbolsLocationsInStringContainer = findLocation(sIn,':');
+        while (specialSymbolsLocationsInStringContainer.size()!=0) {
+            sIn.insert(specialSymbolsLocationsInStringContainer.back(),"\\");
+            specialSymbolsLocationsInStringContainer.pop_back();
+        }
+
+        // MADNESS ENDS
+
+        if (sIn[sIn.length()-1] == '?' or sIn[sIn.length()-1] == ':' or sIn[sIn.length()-1] == '.') { // if it's question
             qIn.number++;
             qIn.name = sIn;
             if (qIn.number != 1) {fOut << "}" << endl << endl;} // if not first question
             fOut << "::Вопрос " << to_string(qIn.number) << "::" << qIn.name <<"{"<<endl;
 
         } else { // else it's answer
-            if (sIn[0] == '+') { sIn.erase(0,1); fOut << "=" << sIn << endl; plCount++;} // if right answer then type it out
-            else { fOut << "~" << sIn << endl;} // if wrong answer then type it out
+            if (sIn[0] == '+') { sIn.erase(0,1); fOut << "=" << sIn << endl; plCount++;} // if right answer
+            else { fOut << "~" << sIn << endl;} // if wrong answer
         }
         getline(fIn,sIn);
+        stringCounter++; //!!!!!!!!
         while (sIn[0]==' ') {sIn.erase(0,1);}// clear leading spaces
         while (sIn[sIn.length()-1] == ' ') {sIn.pop_back();} // clear trailing spaces
     }
