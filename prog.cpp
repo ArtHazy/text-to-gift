4b3c555e1b79490fd0fa9e2d9bac6641eb8e0248
Art Hyena <90830990+Artechigh@users.noreply.github.com>
2023-02-06 22:33:07 +0000

    fix upd

--- prog.cpp
+++ prog.cpp
@@ -10,29 +10,34 @@ struct question{
     string name;
 };
 int main() {
-    fstream fileIn, fileOut;
-    fileIn.open("text.txt");
-    fileOut.open("result.txt", std::ofstream::out | std::ofstream::trunc);
-    string stringIn;
-    question questionIn;
-
-    string titleOut;
+    fstream fIn, fOut;
+    fIn.open("textIn.txt");
+    fOut.open("giftOut.txt", std::ofstream::out | std::ofstream::trunc);
+    string sIn="lol"; // string In
+    question qIn; // question in
+    int plCount = 0; // plus's counter
     string answer;
-    getline(fileIn,stringIn); // got 1-st line
+    getline(fIn,sIn); // got 1-st line
+    while (sIn[0]==' ') {sIn.erase(0,1);}// clear leading spaces
+    while (sIn[sIn.length()-1] == ' ') {sIn.pop_back();} // clear trailing spaces
 
-    while ( /*questionIn.number<2*/ stringIn!="end") { // while there are strings left // 2 - number of questions
-        if (stringIn[stringIn.length()-1] == '?' or stringIn[stringIn.length()-1] == ':' or stringIn[stringIn.length()-1] == '.') { // if found question text then type it out
-            questionIn.number++;
-            questionIn.name = stringIn;
-            fileOut << "}" << endl;
-            fileOut << "::Вопрос " << to_string(questionIn.number) << "::" << questionIn.name <<"{"<<endl;
+    while (sIn!="") { // while there are no empty strings
+        if (sIn[sIn.length()-1] == '?' or sIn[sIn.length()-1] == ':' or sIn[sIn.length()-1] == '.') { // if found question text then type it out
+            qIn.number++;
+            qIn.name = sIn;
+            if (qIn.number != 1) {fOut << "}" << endl << endl;} // if not first question
+            fOut << "::Вопрос " << to_string(qIn.number) << "::" << qIn.name <<"{"<<endl;
 
         } else { // else it's answer
-            if (stringIn[0] == '+') { fileOut << "=" << stringIn << endl;} // if right answer then type it out
-            else { fileOut << "∼" << stringIn << endl;} // if wrong answer then type it out
+            if (sIn[0] == '+') { sIn.erase(0,1); fOut << "=" << sIn << endl; plCount++;} // if right answer then type it out
+            else { fOut << "~" << sIn << endl;} // if wrong answer then type it out
         }
-        getline(fileIn,stringIn);
+        getline(fIn,sIn);
+        while (sIn[0]==' ') {sIn.erase(0,1);}// clear leading spaces
+        while (sIn[sIn.length()-1] == ' ') {sIn.pop_back();} // clear trailing spaces
     }
-    
+    fOut << "}";
+    cout << qIn.number << " questions found" << endl;
+    cout << plCount << " correct answers found" << endl;
     return 0;
 }
