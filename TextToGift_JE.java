e8dad8a404b89431f901927268839501d39ce6b9
Art Monakhov <artemius135@gmail.com>
2023-10-01 23:23:02 +0300

    TTG_JE 0.12

--- TextToGift_JE.java
+++ TextToGift_JE.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 
@@ -6,11 +7,13 @@ import java.util.ArrayList;
 class Question {
     int number;
     String name;
+    boolean isWriteDownQuestion;
 
     Question() {
         this.number = 0;
     }
 }
+
 
 public class TextToGift_JE {
     
@@ -22,6 +25,60 @@ public class TextToGift_JE {
         }
         return false;
         
+    }
+
+    static boolean isWriteDownQuestion(String sIn) {
+        if (IsQuestion(sIn)){
+            if (sIn.charAt(0)=='+'){return true;}
+        }
+        return false;
+    }
+
+    static boolean isChoiceQuestion(String sIn) {
+        return !isWriteDownQuestion(sIn);
+    }
+
+    static void handleWriteDownQuestion(ArrayList<String> variants, BufferedWriter fOut) throws IOException{
+        while (!variants.isEmpty()) {
+            String bufString = variants.get(variants.size() - 1);
+            variants.remove(variants.size() - 1);
+            if (bufString.charAt(0)=='+'){bufString=bufString.substring(1);}
+            fOut.write("=" + bufString.trim() + "\n");
+        }
+    }
+
+    static void handleSingleChoiseQuestion(ArrayList<String> variants, BufferedWriter fOut) throws IOException{
+        while (!variants.isEmpty()) {
+            String bufString = variants.get(variants.size() - 1);
+            variants.remove(variants.size() - 1);
+
+            if (bufString.charAt(0) == '+') {
+                bufString = bufString.substring(1);
+                fOut.write("=" + bufString.trim() + "\n");
+            } else {
+                 fOut.write("~" + bufString.trim() + "\n");
+            }
+        }
+    }
+
+    static void handleMultipleChoiseQuestion(ArrayList<String> variants, int correctVariants, BufferedWriter fOut) throws IOException{
+        int variantsOverall = variants.size();
+        while (!variants.isEmpty()) {
+            String bufString = variants.get(variants.size() - 1);
+            variants.remove(variants.size() - 1);
+
+            double points = 0;
+            DecimalFormat pointsFormat = new DecimalFormat("0.###");
+
+            if (bufString.charAt(0) == '+') {
+                bufString = bufString.substring(1);
+                points = (double)100/correctVariants;
+                fOut.write("~" + "%" + pointsFormat.format(points) + "%" + bufString.trim() + "\n");
+            } else {
+                points = (100 / ((double) variantsOverall - (double) correctVariants));
+                fOut.write("~" + "%" + "-" + pointsFormat.format(points) + "%" + bufString.trim() + "\n");
+            }
+        }
     }
 
     static ArrayList<Integer> findLocation(String sample, char findIt) {
@@ -39,40 +96,65 @@ public class TextToGift_JE {
         char[] specialChars = {'~', '=', '#', '{', '}', ':'};
 
         while ((line = inFile.readLine()) != null) {
-            line = removeBulletedLists(line);
-            for (char c : specialChars) {
-                ArrayList<Integer> locations = findLocation(line, c);
-                while (!locations.isEmpty()) {
-                    int index = locations.get(locations.size() - 1);
-                    line = line.substring(0, index) + "\\" + line.substring(index);
-                    locations.remove(locations.size() - 1);
+            line = line.trim();
+            if (!(line.length()>=2 && line.substring(0, 2).equals("//"))){
+                line = removeBulletedLists(line);
+                for (char c : specialChars) {
+                    ArrayList<Integer> locations = findLocation(line, c);
+                    while (!locations.isEmpty()) {
+                        int index = locations.get(locations.size() - 1);
+                        line = line.substring(0, index) + "\\" + line.substring(index);
+                        locations.remove(locations.size() - 1);
+                    }
                 }
+                if ((line.length()!=0) && (line.charAt(line.length()-1)==';' || (line.charAt(line.length()-1)==',') || (line.charAt(line.length()-1)=='.'))){
+                    try {
+                        line = line.substring(0, line.length()-1);
+                    } catch (Exception e) {
+                        // TODO: handle exception
+                    }
+                }
+                outFile.write(line + "\n");
             }
-            outFile.write(line.trim() + "\n");
+            
         }
         
         System.out.println("Special symbols were cleared");
     }
 
     static String removeBulletedLists(String mystring) {
-        String substring = mystring.substring(0, 3);
-        if (substring.charAt(1) == ')' && substring.charAt(2) == ' ') {
-            return mystring.substring(3);
-        } else {
-            return mystring;
+        try {
+            String substring = mystring.substring(0, 3);
+            if (substring.charAt(1) == ')' && substring.charAt(2) == ' ') {
+                return mystring.substring(3);
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
         }
+        return mystring;
+        
     }
 
     static void formGift(BufferedReader fIn, BufferedWriter fOut) throws IOException {
-        int singleAnswerQuestionsCounter = 0;
-        int multipleAnswerQuestionsCounter = 0;
+        int singleChoiceCount=0, multipleChoicesCount=0, writeDownCount=0;
 
         String sIn;
-        Question bufQuestion = new Question();
         sIn = fIn.readLine();
+
+        Question bufQuestion = new Question();
+        
+
         while (sIn != null && sIn!=null && !sIn.equals("")) {
 
             while (IsQuestion(sIn)) {
+
+                if (isWriteDownQuestion(sIn)){
+                    bufQuestion.isWriteDownQuestion=true;
+                    sIn = sIn.substring(1);
+                } else {
+                    bufQuestion.isWriteDownQuestion=false;
+                }
+
                 bufQuestion.name = sIn;
                 bufQuestion.number++;
 
@@ -82,49 +164,34 @@ public class TextToGift_JE {
                 int correctVariants = 0;
 
                 sIn = fIn.readLine();
+
                 while (sIn != null && !IsQuestion(sIn) && sIn!=null && !sIn.equals("")) {
                     if (sIn.charAt(0) == '+') {
                         correctVariants++;
                     }
                     variants.add(sIn);
+
                     sIn = fIn.readLine();
                 }
 
-                switch (correctVariants) {
+                if (bufQuestion.isWriteDownQuestion){
+                    writeDownCount++;
+                    handleWriteDownQuestion(variants,fOut);
+                }
+                if (!(bufQuestion.isWriteDownQuestion)){
+                    switch (correctVariants) {
                     case 0:
                         System.out.println("Question " + bufQuestion.number + " has no correct answers!");
                         break;
 
                     case 1:
-                        singleAnswerQuestionsCounter++;
-                        while (!variants.isEmpty()) {
-                            String bufString = variants.get(variants.size() - 1);
-                            variants.remove(variants.size() - 1);
-
-                            if (bufString.charAt(0) == '+') {
-                                bufString = bufString.substring(1);
-                                fOut.write("=" + bufString + "\n");
-                            } else {
-                                fOut.write("~" + bufString + "\n");
-                            }
-                        }
+                        singleChoiceCount++;
+                        handleSingleChoiseQuestion(variants,fOut);
                         break;
-
                     default:
-                        multipleAnswerQuestionsCounter++;
-                        int allVariants = variants.size();
-                        while (!variants.isEmpty()) {
-                            String bufString = variants.get(variants.size() - 1);
-                            variants.remove(variants.size() - 1);
-
-                            if (bufString.charAt(0) == '+') {
-                                bufString = bufString.substring(1);
-                                fOut.write("~" + "%" + 100 / (float) correctVariants + "%" + bufString + "\n");
-                            } else {
-                                fOut.write("~" + "%" + "-" + 100 / ((float) allVariants - (float) correctVariants) + "%" + bufString + "\n");
-                            }
-                        }
-                        break;
+                        multipleChoicesCount++;
+                        handleMultipleChoiseQuestion(variants,correctVariants,fOut);
+                }
                 }
 
                 fOut.write("}" + "\n" + "\n");
@@ -133,8 +200,9 @@ public class TextToGift_JE {
 
         System.out.println("Gift was formated");
         System.out.println("Questions found overall: " + bufQuestion.number);
-        System.out.println("1-answer questions found: " + singleAnswerQuestionsCounter);
-        System.out.println("mult-answer questions found: " + multipleAnswerQuestionsCounter);
+        System.out.println("1-answer questions found: " + singleChoiceCount);
+        System.out.println("mult-answer questions found: " + multipleChoicesCount);
+        System.out.println("Write down question found: " + writeDownCount);
     }
 
     public static void main(String[] args) throws IOException {
